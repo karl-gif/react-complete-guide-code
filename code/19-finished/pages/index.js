61015156ee07fd2c1c87daab2d6497c08d1f358c
edgkar <karl.edge@gmail.com>
2022-01-29 17:22:59 +0000

    add thread lock

--- code/19-finished/pages/index.js
+++ code/19-finished/pages/index.js
@@ -1,13 +1,34 @@
 import { Fragment } from 'react';
 import Head from 'next/head';
-//import { MongoClient } from 'mongodb';
+import { useMutex } from 'react-context-mutex';
 
 import MeetupList from '../components/meetups/MeetupList';
 
 let count = 1;
 
+function sleep(delay) {
+  var start = new Date().getTime();
+  while (new Date().getTime() < start + delay);
+}
+
+const useFetchHook = () => {
+  const MutexRunner = useMutex();
+  const mutex = new MutexRunner('pause');
+
+  mutex.run(async () => {
+      mutex.lock();
+      try {
+          sleep(2000);          
+          mutex.unlock();
+      } catch (e) {
+          mutex.unlock();
+      }
+  });
+};
+
 function HomePage(props) {
-  return (
+  {useFetchHook()}
+  return (    
     <Fragment>
       <Head>
         <title>Pets</title>
@@ -21,31 +42,7 @@ function HomePage(props) {
   )
 }
 
-// export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
-
-//   // fetch data from an API
-
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS
-//     }
-//   };
-// }
-
 export async function getStaticProps() {
-  // fetch data from an API
-  // const client = await MongoClient.connect(
-  //   'mongodb+srv://maximilian:TU6WdZF2EjFWsqUt@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority'
-  // );
-  // const db = client.db();
-
-  // const meetupsCollection = db.collection('meetups');
-
-  // const meetups = await meetupsCollection.find().toArray();
-
-  // client.close();
 
   console.log("Refresh: " + count);
 
